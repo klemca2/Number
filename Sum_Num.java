bc3c248820f8a580eba34e0b6330f086b54fdcc8
klemca2 <klemca2@kletech.ac.in>
2024-05-23 11:45:40 +0530

    simple code

---
+++ Sum_Num.java
@@ -0,0 +1,11 @@
+public class Sum_Num{
+    public static void main(String[] args) {
+        int num=43;
+        if(num%2==0){
+            System.out.println("Entered Number is Even");
+        }
+        else{
+            System.out.println("Entered Number is Odd");
+        }
+    }
+}
